745c7273637846cd204726abecc6975ea45c80fe
Omer Munk <omermunk95@gmail.com>
2024-09-08 14:51:11 +0300

    function init

--- intro/filter_ex.py
+++ intro/filter_ex.py
@@ -8,8 +8,22 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # function, iterable
 # the function needs to be a function that returns true a false
 # the iterable - an object that we can iterate on.
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
-print(even_numbers)
+print(even_numbers) # [2, 4, 6, 8, 10]
 
 
-# () => {}
+vowels = ['a', 'e', 'i', 'o', 'u']
+str1= "Hello world"
+filtered_with_no_vowels = filter(lambda x: x not in vowels, str1)
+print(list(filtered_with_no_vowels))
+
+
+# create a list with 10 names
+# filter only the words with maximum length of 5
+words = ["Abraha", "avi", "seth", "shlomo", "shalom", "gadi", "david", "dud"]
+MAX_LENGTH = 5
+
+short_words = list(filter(lambda word: len(word) <= MAX_LENGTH, words))
+
+
---
+++ intro/functions.py
@@ -0,0 +1,23 @@
+"""
+function that gets 2 ints and returns the result divided between them in double
+
+public static double Devide2Numbers(int num1, int num2){
+    ....
+    return result
+}
+"""
+# rules:
+"""
+1. start with def
+2. name of function
+3. ()
+4. parameters
+5. :
+6. logic inside WITH INDENTATION
+"""
+
+def divide_2_numbers(num1, num2):
+    result = num1 / num2
+    return result
+
+
