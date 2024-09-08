56b33059f73c0ecbd5f333d70fbafb9b055fb05d
Omer Munk <omermunk95@gmail.com>
2024-09-08 12:37:23 +0300

    loops solutions

--- intro/loops.py
+++ intro/loops.py
@@ -60,3 +60,109 @@ ex6: create a code that counts the vowels of a word
 
 """
 
+
+
+# ex1
+fruits = ['apple', 'orange', 'pear', "strawberry", "peach"]
+for fruit in fruits:
+    print(f"fruit is: {fruit}")
+
+
+# ex2
+fruits = ['apple', 'orange', 'pear', "strawberry", "peach"]
+for i in range(len(fruits)):
+    print(f"fruit in index {i} is: {fruits[i]}")
+
+
+# ex3 1
+grades = [100, 90, 87, 88, 0, 75, 100, 90, 89, 30]
+count = 0
+sum_grades = 0
+for grade in grades:
+    count += 1
+    sum_grades += grade
+avg = sum_grades / count
+print(avg)
+
+# ex3 2
+grades = [100, 90, 87, 88, 0, 75, 100, 90, 89, 30]
+sum_grades = 0
+for grade in grades:
+    sum_grades += grade
+avg = sum_grades / len(grades)
+print(avg)
+
+# ex3 2
+grades = [100, 90, 87, 88, 0, 75, 100, 90, 89, 30]
+sum_grades = sum(grades)
+avg = sum_grades / len(grades)
+print(avg)
+
+# ex4
+grades = [100, 90, 87, 88, 0, 75, 100, 90, 89, 30]
+maximum = grades[0]
+for grade in grades:
+    if grade > maximum:
+        maximum = grade
+print(maximum)
+
+maximum = max(grades)
+
+grades.sort()
+print(grades[-1])
+
+
+#ex 5
+for i in range(10):
+    print(f"5 x {i+1} = {5 * (i + 1)}")
+
+
+str1 = "Hello Kodcode, Shubulu"
+vowels = "aeiou"
+vowels += vowels.upper()
+count = 0
+for char in str1:
+    if char in vowels:
+        count += 1
+
+print(f"the amount of vowels is: {count}")
+
+
+
+
+# break continue
+
+# if we want to iterate over all the number 0-9
+# stop when we get to 5 (not included)
+# print odd numbers only
+# use for, in, range, if, elif, break, continue
+
+
+for num in range(10):
+    if num == 5:
+        break
+    elif num % 2 == 0:
+        continue
+    print(num)
+
+
+
+# create a loop, that finds all the prime numbers from 2 to 25
+# prime number is a number that can be divided only by 1 and itself.
+for num in range(2, 260000):
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(f"{num} is a prime number")
+
+print("FINISH!")
+
+
+
+
+
+
+
