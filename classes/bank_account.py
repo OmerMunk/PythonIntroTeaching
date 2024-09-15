7131dd27b572e12a4bb80ec09b2f201674e50fbe
Omer Munk <omermunk95@gmail.com>
2024-09-15 11:06:35 +0300

    decorator

--- classes/bank_account.py
+++ classes/bank_account.py
@@ -2,13 +2,40 @@ from datetime import datetime
 
 
 class BankAccount:
-
     monthly_interest = 0.10
+    vip_minimum = 1_000_000
 
     def __init__(self, owner, balance=0, vip=False):
         self.owner = owner # public
         self._balance = balance # protected
         self.__vip = vip # private
+
+    @property
+    def vip(self):
+        return self.__vip
+
+    @vip.setter
+    def vip(self, value):
+        if self._balance >= BankAccount.vip_minimum:
+            self.__vip = value
+
+
+    def __str__(self):
+        return f'owner: {self.owner}, balance: {self._balance}, vip: {self.__vip}'
+
+    def __repr__(self):
+        return f'{self.owner}:{self._balance}'
+
+    def __add__(self, other):
+        if isinstance(other, BankAccount):
+            return self._balance + other._balance
+
+
+
+
+    # def __repr__(self):
+
+
 
     def deposit(self, amount):
         self._balance += amount
@@ -29,7 +56,7 @@ class BankAccount:
         cls.monthly_interest = new_interest
 
     @staticmethod
-    def papars_200(money):
+    def papers_200(money):
         return money // 200
 
 
@@ -42,7 +69,6 @@ class BankAccount:
 if __name__ == '__main__':
     account1 = BankAccount('Pahshish', -100)
     account2 = BankAccount('Zohar')
-    account21 = BankAccount('Zohar', 400, False)
     account3 = BankAccount('Zohar', vip=True)
     account4= BankAccount('Zohar', balance=400, vip=True)
     account5 = BankAccount('Zohar', vip=True, balance=500)
@@ -58,4 +84,16 @@ if __name__ == '__main__':
 
     print(account1.owner)
     print(account1._balance)
-    print(account1.__vip)
+    # print(account1.__vip)
+
+
+    account21 = BankAccount('Zohar', 400, False)
+    print(account21)
+    account1 = BankAccount('Pahshish', -100)
+    print(account1 + account21)
+    print([account1, account21])
+    print(account1.vip)
+    account1.vip = True
+    print(account1.vip)
+
+
--- classes/shapes.py
+++ classes/shapes.py
@@ -1,4 +1,6 @@
-class Shape:
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
     def __init__(self, nodes, color):
         self.nodes = nodes
         self.color = color
@@ -6,8 +8,31 @@ class Shape:
     def print_color(self):
         print(f'color: {self.color}')
 
-class Puki:
-    pass
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, width, height, color):
+        super().__init__(4, color)
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+
+class Rhombus(Shape):
+    def __init__(self, length, diagonal1, diagonal2,color):
+        super().__init__(4, color)
+        self.length = length
+        self.diagonal1 = diagonal1
+        self.diagonal2 = diagonal2
+
+    def calculate_area(self):
+        return  self.diagonal1 * self.diagonal2 / 2
+
+
+
 
 
 class Circle(Shape):
@@ -15,7 +40,7 @@ class Circle(Shape):
         super().__init__(0, color)
         self.radius = radius
 
-class Square(Shape):
+class Square(Rectangle, Rhombus):
     def __init__(self, length, color):
         super().__init__(4, color)
         self.length = length
