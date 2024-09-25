ba9e31d1d427eabd6423db764ddc16b47d2d727b
Omer Munk <omermunk95@gmail.com>
2024-09-25 10:01:16 +0300

    postgres

--- postgres_proj/main.py
+++ postgres_proj/main.py
@@ -23,17 +23,20 @@ if __name__ == '__main__':
         )
         print("Connected to the database")
         cur = conn.cursor()
-        cur.execute("SELECT * FROM cars;")
 
-
-        print(type(cur))
+        # this is how to make a query and fetch all the results
+        name_to_search = input("Hi, I am so naive, enter the name you want me to search")
+        cur.execute(f"SELECT * FROM clients where cname = '{name_to_search}';")
         rows = cur.fetchall()
         for row in rows:
             print(row)
 
 
     except psycopg2.Error as e:
-        print("Error: Could not make connection to the Postgres database")
+        print("Error: Could not make connection to the Postgres database"
+              "or execute the query")
+        print(e)
+    except Exception as e:
         print(e)
     finally:
         if cur:
